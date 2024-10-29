e75cdb9ba1469ba7d0a6e6407fe3677264957c80
Lorenzo Salvadore <salvadore@FreeBSD.org>
2024-10-29 13:01:31 +0100

    Add -p option
    Add -p option to allow to update only a few specific ports and their
    dependencies.

--- main.cpp
+++ main.cpp
@@ -12,25 +12,32 @@ void Usage()
 {
     cout  << "Usage: poesis [-h] [-n]" << endl
           << "-h      Print this help message" << endl
+          << "-p <port origin>      Update the port with origin <origin> and its dependencies" << endl
           << "-n      Dry-run: only print what needs to be updated" << endl;
 }
 
 int main(int argc, char *argv[])
 {
-  bool DryRun = false;
+  bool DryRun = false,
+       PortSpecified = false;
   char Buffer[128];
   FILE *Pipe;
   unordered_map<string,Package> Outdated;
+  string SpecifiedPortsList;
 
   {
     int Option;
-    while((Option = getopt(argc, argv, "hn")) != -1)
+    while((Option = getopt(argc, argv, "hp:n")) != -1)
     {
       switch(Option)
       {
         case 'h':
           Usage();
           return 0;
+        case 'p':
+          PortSpecified = true;
+          SpecifiedPortsList = optarg;
+          break;
         case 'n':
           DryRun = true;
           break;
@@ -62,22 +69,37 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
+  {
+    if(DryRun) cout << "=== Outdated ports:" << endl;
+    while(fgets(Buffer,128,Pipe))
     {
-      if(DryRun) cout << "=== Outdated ports:" << endl;
-      while(fgets(Buffer,128,Pipe))
+      Buffer[strlen(Buffer) - 1] = 0; // remove \n
+      string PackageOrigin = Buffer;
+      cout << "=== Retrieving data for outdated port " + PackageOrigin + "..." << endl;
+      Outdated.emplace(PackageOrigin,Package(PackageOrigin,Outdated));
+    }
+  }
+  pclose(Pipe);
+
+  if(PortSpecified)
+  {
+    for(auto Iterator: Outdated)
+    {
+      if(SpecifiedPortsList.find(Iterator.first) != string::npos)
       {
-        Buffer[strlen(Buffer) - 1] = 0; // remove \n
-        string PackageOrigin = Buffer;
-        cout << "=== Retrieving data for outdated port " + PackageOrigin + "..." << endl;
-        Outdated.emplace(PackageOrigin,Package(PackageOrigin,Outdated));
+         if(DryRun) (Iterator.second).SimulateUpdate();
+         else (Iterator.second).Update();
       }
     }
-    pclose(Pipe);
+  }
+  else
+  {
     for(auto Iterator: Outdated)
     {
       if(DryRun) (Iterator.second).SimulateUpdate();
       else (Iterator.second).Update();
     }
+  }
 
   return 0;
 }
